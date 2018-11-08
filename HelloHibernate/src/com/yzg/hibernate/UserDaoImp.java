19a1fb71259d70ed88b9dcf8f93a7450f607073a
NElight <15225160180@163.com>
2018-11-08 18:31:41 +0800

    hibernate 1108

--- HelloHibernate/src/com/yzg/hibernate/UserDaoImp.java
+++ HelloHibernate/src/com/yzg/hibernate/UserDaoImp.java
@@ -1,21 +1,21 @@
 package com.yzg.hibernate;
 
-import javax.transaction.HeuristicMixedException;
-import javax.transaction.HeuristicRollbackException;
-import javax.transaction.RollbackException;
-import javax.transaction.SystemException;
-import javax.transaction.Transaction;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.jws.soap.SOAPBinding.Use;
 
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class UserDaoImp {
 	
 	private static SessionFactory factory;
 	
-	public static void main(String args[]) throws IllegalStateException, SecurityException, SystemException, RollbackException, HeuristicMixedException, HeuristicRollbackException {
+	public static void main(String args[]) {
 		try {
 			factory = new Configuration().configure().buildSessionFactory();
 		} catch (Exception e) {
@@ -25,20 +25,30 @@ public class UserDaoImp {
 		}
 		
 		UserDaoImp me = new UserDaoImp();
-		Integer r1 = me.addUser(1, "yzg", "123456");
 		
-		System.out.println(r1);	
+//		Integer r1 = me.addUser(1, "yzg", "123456");
+//		Integer r2 = me.addUser(2, "aljdf", "djfalfja");
+//		
+//		System.out.println(r1);	
+//		System.out.println(r2);
+		
+//		me.updateUser(1, "abcd", "efgh");
+		me.deleteUser(21);
+		me.deleteUser(20);
+		
+		me.listUser();
+		factory.close();
 	}
 	
 	
-	public Integer addUser(int id, String name, String password) throws IllegalStateException, SystemException, SecurityException, RollbackException, HeuristicMixedException, HeuristicRollbackException {
+	public Integer addUser(int id, String name, String password) {
 		
 		Session session = factory.openSession();
 		Transaction tx = null;
 		Integer relid = null;
 		try {
-			tx = (Transaction) session.beginTransaction();
-			User user = new User(1, "yzg", "123456");
+			tx = session.beginTransaction();
+			User user = new User(id, name, password);
 			relid = (Integer) session.save(user);
 			tx.commit();
 		}catch (HibernateException e) {
@@ -50,10 +60,67 @@ public class UserDaoImp {
 			session.close();
 		}
 		
-		return relid;
-		
-		
-		
+		return relid;	
+	}
+	
+	public void listUser() {
+		Session session = factory.openSession();
+		Transaction tx = null;
+		try {
+			tx = session.beginTransaction();
+			List users = session.createQuery("from User").list();
+			for (Iterator iterator = users.iterator(); iterator.hasNext();) {
+				User user = (User)iterator.next();
+				System.out.println("id" + user.getId() + "\n" + "name"+ user.getName() + "\n" + "pwd" + user.getPassword() + "\n");
+				
+			}
+			tx.commit();
+		}catch(HibernateException e) {
+			if(tx!=null) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+		}finally {
+			session.close();
+		}
+	}
+	
+	public void updateUser(Integer id, String name, String password) {
+		Session session = factory.openSession();
+		Transaction tx = null;
+		try {
+			tx = session.beginTransaction();
+			User user = (User)session.get(User.class, id);
+			user.setName(name);
+			user.setPassword(password);
+			session.update(user);
+			tx.commit();
+		}catch (HibernateException exception) {
+			if (tx != null) {
+				tx.rollback();
+			}
+			exception.printStackTrace();
+		}finally {
+			session.close();
+		}
+	}
+	
+	public void deleteUser(Integer id) {
+		Session session = factory.openSession();
+		Transaction tx = null;
+		try {
+			tx = session.beginTransaction();
+			User user = (User) session.get(User.class, id);
+			session.delete(user);
+			tx.commit();
+		}catch(HibernateException exception) {
+			if(tx != null) {
+				tx.rollback();
+			}
+			exception.printStackTrace();
+		}finally {
+			session.close();
+		}
 	}
 	
 
